675e81d9d95b4e159a74e609c100830c47579c38
Denis <denis.tyumin228@gmail.com>
2022-08-08 20:01:23 +0300

    updated Seminar_1

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Программа для возведения числа в квадрат.
+
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// // 5^2 = 5 * 5
+
+// int square = number * number;   
+// // double testResult = Math.Pow(number, 2) [Pow - power (число, степень)]
+
+// // "Cw" + Tab выводит System.Console.WriteLine();
+// Console.WriteLine("Результат: " + square); 
+// //конкатенация: к левой строчке приписывается правая
+
+
+
+
+// // Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго
+// // a = 25, b = 5 -> да
+// // a =2, b = 10 -> нет
+
+// Console.Write("Введите первое число ");
+//     int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число ");
+//     int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// if (firstNumber == secondNumber * secondNumber)
+// {
+//     Console.WriteLine(firstNumber + " является квадратом от " + secondNumber);
+// }
+// else
+// {
+//     Console.WriteLine(firstNumber + " НЕ является квадратом от " + secondNumber);
+// }
+
+
+
+
